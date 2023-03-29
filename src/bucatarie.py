30383ae262035862f29dea6d1454f09592773f5f
Munteanu Elvis <64192172+entirelymagic@users.noreply.github.com>
2023-03-29 21:33:33 +0300

    Mesaj nume commit

---
+++ src/bucatarie.py
@@ -0,0 +1,21 @@
+class Bucatarie:
+    """
+    un inventar exemplu
+    """
+    def __init__(self, nume):
+        self.nume = nume
+        self.inventar = {}
+
+
+    def scade_inventar(self, nume, cantitate):
+        """va scadea din inventar ingredientele oferite ca parametru
+        puteti da o lista de ingrediente sau un ingredient
+
+        """
+        pass
+
+    def adauga_inventar(self, lista_ingredient):
+        """
+        Sa se scada din inventar ingredientele respective
+        """
+        pass
